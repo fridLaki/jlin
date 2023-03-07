902236acd946d28fa643f6e5b0573d18db69bda1
fridlaki <wilfriduslaki@gmail.com>
2023-03-07 15:55:52 +0700

    initial commit

---
+++ src/main/java/com/jalin/JalinApplication.java
@@ -0,0 +1,13 @@
+package com.jalin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JalinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JalinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jalin/JalinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JalinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
